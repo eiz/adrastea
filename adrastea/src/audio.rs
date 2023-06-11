2e9c728d010a206a4e44942a6647a3ddd0e8f13e
Mack Straight <mack@str.rs>
2023-06-10 19:07:07 -0700

    lol, have to use latest git because libspa-sys is missing basic stuff

--- adrastea/src/audio.rs
+++ adrastea/src/audio.rs
@@ -13,7 +13,18 @@
  * with Adrastea. If not, see <https://www.gnu.org/licenses/>.
  */
 
-use pipewire::{Context, MainLoop};
+use core::ffi::c_void;
+
+use libspa_sys::{
+    spa_audio_info_raw, spa_format_audio_raw_build, spa_pod_builder, spa_pod_builder_init,
+    SPA_PARAM_EnumFormat, SPA_AUDIO_FORMAT_F32,
+};
+use pipewire::{
+    properties,
+    spa::Direction,
+    stream::{ListenerBuilderT, Stream, StreamFlags},
+    Context, MainLoop,
+};
 
 pub unsafe fn test() -> anyhow::Result<()> {
     let main_loop = MainLoop::new()?;
@@ -23,7 +34,38 @@ pub unsafe fn test() -> anyhow::Result<()> {
 
     let _listener =
         registry.add_listener_local().global(|g| println!("global: {:?}", g)).register();
-
+    let mut stream: Stream<()> = Stream::new(
+        &core,
+        "audio-capture",
+        properties! {
+            *pipewire::keys::MEDIA_TYPE => "Audio",
+            *pipewire::keys::MEDIA_CATEGORY => "Capture",
+            *pipewire::keys::MEDIA_ROLE => "Communication",
+        },
+    )?;
+    let mut builder: spa_pod_builder = std::mem::zeroed();
+    let mut buf = [0u8; 1024];
+    spa_pod_builder_init(&mut builder, buf.as_mut_ptr() as *mut c_void, buf.len() as u32);
+    let pod = spa_format_audio_raw_build(
+        &mut builder,
+        SPA_PARAM_EnumFormat,
+        &mut spa_audio_info_raw { format: SPA_AUDIO_FORMAT_F32, ..std::mem::zeroed() },
+    );
+    let _stream_listener = stream
+        .add_local_listener()
+        .process(|_, _| {
+            println!("process");
+        })
+        .state_changed(|_, _| {
+            println!("state changed");
+        })
+        .register()?;
+    stream.connect(
+        Direction::Input,
+        None,
+        StreamFlags::AUTOCONNECT | StreamFlags::MAP_BUFFERS | StreamFlags::RT_PROCESS,
+        &mut [pod],
+    )?;
     main_loop.run();
     Ok(())
 }
