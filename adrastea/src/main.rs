aea18909d124dd46b84a98630bcb79a108cb00bb
Mack Straight <mack@str.rs>
2023-06-16 22:37:33 -0700

    mouse input

--- adrastea/src/main.rs
+++ adrastea/src/main.rs
@@ -21,9 +21,11 @@ use core::{
     time::Duration,
 };
 use half::f16;
-use std::{collections::HashMap, fs::File, path::Path, time::Instant};
+use std::{collections::HashMap, fs::File, os::fd::RawFd, path::Path, time::Instant};
 use wayland_client::{
-    protocol::{wl_buffer, wl_compositor, wl_registry, wl_shm, wl_shm_pool, wl_surface},
+    protocol::{
+        wl_buffer, wl_compositor, wl_pointer, wl_registry, wl_seat, wl_shm, wl_shm_pool, wl_surface,
+    },
     Connection, Dispatch,
 };
 use wayland_protocols::xdg::shell::client::{xdg_surface, xdg_toplevel, xdg_wm_base};
@@ -1155,6 +1157,10 @@ struct WaylandTest {
     xdg_surface: Option<xdg_surface::XdgSurface>,
     xdg_top_level: Option<xdg_toplevel::XdgToplevel>,
     buffer: Option<wl_buffer::WlBuffer>,
+    memfd: Option<RawFd>,
+    mmap_mut: Option<memmap2::MmapMut>,
+    wl_seat: Option<wl_seat::WlSeat>,
+    wl_pointer: Option<wl_pointer::WlPointer>,
 }
 
 impl WaylandTest {
@@ -1169,6 +1175,10 @@ impl WaylandTest {
             xdg_surface: None,
             xdg_top_level: None,
             buffer: None,
+            memfd: None,
+            mmap_mut: None,
+            wl_seat: None,
+            wl_pointer: None,
         }
     }
 }
@@ -1182,33 +1192,42 @@ impl Dispatch<wl_registry::WlRegistry, ()> for WaylandTest {
             println!("global {:?} {:?} {:?}", name, interface, version);
             if interface == "wl_compositor" {
                 state.compositor = Some(proxy.bind(name, version, qhandle, ()));
-                println!("compositor {:?}", state.compositor);
             }
             if interface == "xdg_wm_base" {
                 state.xdg_wm_base = Some(proxy.bind(name, version, qhandle, ()));
-                println!("got xdg_wm_base");
             }
             if interface == "wl_shm" {
                 state.wl_shm = Some(proxy.bind(name, version, qhandle, ()));
+            }
+            if interface == "wl_seat" {
+                state.wl_seat = Some(proxy.bind(name, version, qhandle, ()));
             }
 
             if !state.initialized
                 && state.compositor.is_some()
                 && state.xdg_wm_base.is_some()
                 && state.wl_shm.is_some()
+                && state.wl_seat.is_some()
             {
                 let compositor = state.compositor.as_ref().unwrap();
                 let xdg_wm_base = state.xdg_wm_base.as_ref().unwrap();
                 let wl_shm = state.wl_shm.as_ref().unwrap();
+                let wl_seat = state.wl_seat.as_ref().unwrap();
                 let surface = compositor.create_surface(qhandle, ());
                 let xdg_surface = xdg_wm_base.get_xdg_surface(&surface, qhandle, ());
                 let xdg_top_level = xdg_surface.get_toplevel(qhandle, ());
+                let pointer = wl_seat.get_pointer(qhandle, ());
                 let memfd =
                     unsafe { libc::memfd_create(b"wl_shm_pool\0".as_ptr() as *const i8, 0) };
                 unsafe {
-                    libc::ftruncate(memfd, 1024 * 1024 * 100);
+                    libc::ftruncate(memfd, 4096 * 4096 * 4);
+                    let mut mapping = memmap2::Mmap::map(memfd).unwrap().make_mut().unwrap();
+                    mapping.as_mut().fill(0xFF);
+                    state.mmap_mut = Some(mapping);
                 }
-                let wl_shm_pool = wl_shm.create_pool(memfd, 1024 * 1024 * 100, qhandle, ());
+                state.memfd = Some(memfd);
+
+                let wl_shm_pool = wl_shm.create_pool(memfd, 4096 * 4096 * 4, qhandle, ());
                 let buffer = wl_shm_pool.create_buffer(
                     0,
                     1920,
@@ -1225,6 +1244,7 @@ impl Dispatch<wl_registry::WlRegistry, ()> for WaylandTest {
                 state.xdg_top_level = Some(xdg_top_level);
                 state.wl_shm_pool = Some(wl_shm_pool);
                 state.buffer = Some(buffer);
+                state.wl_pointer = Some(pointer);
                 state.initialized = true;
             }
         }
@@ -1295,7 +1315,31 @@ impl Dispatch<xdg_toplevel::XdgToplevel, ()> for WaylandTest {
     ) {
         println!("xdg_toplevel {:?}", event);
         match event {
-            xdg_toplevel::Event::Configure { width, height, states } => {}
+            xdg_toplevel::Event::Configure { width, height, states } => {
+                if width != 0 && height != 0 {
+                    let old_buffer = state.buffer.take().unwrap();
+                    let wl_shm_pool = state.wl_shm_pool.as_ref().unwrap();
+                    let buffer = wl_shm_pool.create_buffer(
+                        0,
+                        width,
+                        height,
+                        width * 4,
+                        wl_shm::Format::Abgr8888,
+                        qhandle,
+                        (),
+                    );
+                    old_buffer.destroy();
+                    let mapping = state.mmap_mut.as_mut().unwrap();
+                    let pixels = unsafe {
+                        std::slice::from_raw_parts_mut(
+                            mapping.as_mut_ptr() as *mut u32,
+                            mapping.len() / 4,
+                        )
+                    };
+                    pixels[..width as usize * height as usize].fill(0x000000FF);
+                    state.buffer = Some(buffer);
+                }
+            }
             xdg_toplevel::Event::Close => {}
             xdg_toplevel::Event::ConfigureBounds { width, height } => {}
             xdg_toplevel::Event::WmCapabilities { capabilities } => {}
@@ -1335,6 +1379,26 @@ impl Dispatch<wl_buffer::WlBuffer, ()> for WaylandTest {
     }
 }
 
+impl Dispatch<wl_pointer::WlPointer, ()> for WaylandTest {
+    fn event(
+        state: &mut Self, proxy: &wl_pointer::WlPointer,
+        event: <wl_pointer::WlPointer as wayland_client::Proxy>::Event, data: &(),
+        conn: &Connection, qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        println!("wl_pointer {:?}", event);
+    }
+}
+
+impl Dispatch<wl_seat::WlSeat, ()> for WaylandTest {
+    fn event(
+        state: &mut Self, proxy: &wl_seat::WlSeat,
+        event: <wl_seat::WlSeat as wayland_client::Proxy>::Event, data: &(), conn: &Connection,
+        qhandle: &wayland_client::QueueHandle<Self>,
+    ) {
+        println!("wl_seat {:?}", event);
+    }
+}
+
 fn wayland_test() -> anyhow::Result<()> {
     let conn = Connection::connect_to_env()?;
     let display = conn.display();
@@ -1346,7 +1410,6 @@ fn wayland_test() -> anyhow::Result<()> {
     loop {
         event_queue.blocking_dispatch(&mut state)?;
     }
-    todo!()
 }
 
 fn main() -> anyhow::Result<()> {
